ee89fd68e912353f0a7882207d1429f836609724
Rohit Kumar <88289975+SigmaQuasar@users.noreply.github.com>
2022-12-09 16:16:57 +0530

    multilevel inheritance learnt today

--- class multilevel inheritance.py
+++ class multilevel inheritance.py
@@ -1,13 +1,27 @@
-class father:
-    def father_eaning(self):
-        print("Education money is on me")
+#Multilevel inheritance between gradfather👴, father🧔‍♂️and the son👶
+
+class Grandpa:
     
-class son(father):
-    def son_earning(self):
+    def __init__(self):
+        self.family_occupation = "Farming 🧑‍🌾"
+                
+    def grandpa_occupation(self):
+        self.occupation = "Headmaster 👨‍🏫"
+        return self.occupation
+
+class Father(Grandpa):
+    def father_occupation(self):
+        self.occupation = "Gov employee"
+        return self.occupation
+    
+class Son(Father):
+    def son_occupation(self):
         print("I am earning to grow")
         
-rohit = son()
-rohit.father_eaning()
-rohit.son_earning()
+rohit = Son()
+print(rohit.grandpa_occupation())
+print(rohit.father_occupation())
+rohit.son_occupation()
+print(rohit.family_occupation)
 
     
