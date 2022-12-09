fee0a8f3a4824af21e085d8a92b9610af5f2d836
Rohit Kumar <88289975+SigmaQuasar@users.noreply.github.com>
2022-12-09 16:30:57 +0530

    learnt multiple inheritance

--- multiple inheritance.py
+++ multiple inheritance.py
@@ -1,1 +1,18 @@
-# Multiple inheritance is one child :child
+# Multiple inheritance is one child🧒 class inheriting from multiple 
+# classes say father🧔‍♂️ and mother 👩
+#
+
+class Mom:
+    def affections(self):
+        print("Mother cares immensely for you")
+        
+class Father:
+    def money(self):
+        print("Father is providing money")
+        
+class Son(Father, Mom):
+    pass
+
+rohit = Son()
+rohit.affections()
+rohit.money()
