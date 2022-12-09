9a6745de2f8462fce105127a6dfe7908f0b4dceb
Rohit Kumar <88289975+SigmaQuasar@users.noreply.github.com>
2022-12-09 15:44:43 +0530

    first multilevel inheritance commit

---
+++ class multilevel inheritance.py
@@ -0,0 +1,13 @@
+class father:
+    def father_eaning(self):
+        print("Education money is on me")
+    
+class son(father):
+    def son_earning(self):
+        print("I am earning to grow")
+        
+rohit = son()
+rohit.father_eaning()
+rohit.son_earning()
+
+    
