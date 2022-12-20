c134ffbd0a67d5101ce55b54b484fbb2c6a7d167
Rohit Kumar <88289975+SigmaQuasar@users.noreply.github.com>
2022-12-20 16:55:01 +0530

    join different threads on multi threading

--- multi threading.py
+++ multi threading.py
@@ -1,16 +1,17 @@
+# Threading 🧵
 import threading
 import time
 
 def eat_popcorn():
-    time.sleep(4)
+    time.sleep(1)
     print("Eating popcorn!")
     
 def watch_movie():
-    time.sleep(3)
+    time.sleep(1)
     print("Watching movie")
     
 def drink_cola():
-    time.sleep(5)
+    time.sleep(1)
     print("Drinking cola!")
     
 e = threading.Thread(target=eat_popcorn)
@@ -21,6 +22,10 @@ w.start()
 
 d = threading.Thread(target=drink_cola)
 d.start()
+
+e.join()
+w.join()
+d.join()
 
 print(threading.active_count())
 print(threading.enumerate())
