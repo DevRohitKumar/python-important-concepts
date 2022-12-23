c4661072b101a39d5efaaa64c1d307ae4a15fdba
Rohit Kumar <88289975+SigmaQuasar@users.noreply.github.com>
2022-12-23 14:13:34 +0530

    tkinter GUI on progress!

--- tkinter try.py
+++ tkinter try.py
@@ -1,5 +1,6 @@
 # First GUI using tkinter 
 from tkinter import *
+from tkinter import messagebox
 
 tk = Tk()
 
@@ -33,12 +34,47 @@ label = Label(tk,
               image=icon,
               compound= "bottom",
               )
-label.pack() #we can also use place(x= , y=) for specific location
+
+# show any item (here this label) on window with pack() or 
+# place(x= , y=) for specific location
+label.pack() 
 
 # configure window 
 tk.config(
     background= "#1664e0"
 )
+
+# Create button
+def click_hi():
+  if (hi_btn['state'] == NORMAL):
+    hi_msg = "Hi there! Nice to meet you.👋 "
+    print(hi_msg)
+    hi_btn['state'] = ACTIVE
+    hi_btn['text'] = "Saying hi!"
+    return messagebox.showinfo('Saying hi!', hi_msg)
+  else:
+    # hi_btn[]
+    hi_btn['state'] = NORMAL
+    hi_btn['text'] = "Say hi.."
+  
+hi_btn = Button(tk,
+             text="Say hi..",
+             command=click_hi,
+             )
+
+hi_btn.config(bg="green",
+           fg="white",
+           activebackground="red",
+           activeforeground="black",
+           state=NORMAL,
+                 
+           )
+
+
+hi_btn.pack()
+
+
+
 
 
 
